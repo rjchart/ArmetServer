9175497902b2ec747385077fa33040812d2350a7
Jaehee Rhee <rjchaos@gmail.com>
2015-06-15 15:56:19 +0900

    에러 처리를 위한 테스

--- app.js
+++ app.js
@@ -5,38 +5,38 @@ var express = require('express');
 var app = express();
 
 
-app.use (express.static(_dirname + '/public'));
+// app.use (express.static(_dirname + '/public'));
 // 미들웨어를 생성합니다.
-// app.use('/', function (req, res, next) {
-	// 기본적인 기능 테스트
-	// res.send('Hello Node.js and Express!');
+app.use(function (req, res, next) {
+	기본적인 기능 테스트
+	res.send('Hello Node.js and Express!');
 
-	// 출력 테스트
-	// var output = [];
-	// for (var i = 0; i < 3; i++) {
-	// 	output.push({
-	// 		count: i,
-	// 		name: 'name - ' + i
-	// 	})
-	// }
-	// res.send(output);
+	출력 테스트
+	var output = [];
+	for (var i = 0; i < 3; i++) {
+		output.push({
+			count: i,
+			name: 'name - ' + i
+		})
+	}
+	res.send(output);
 
-	// 요청 헤더의 속성 추출
-	// var agent = req.header('User-Agent');
-	// // 브라우저 구분
-	// if (agent.toLowerCase().match(/safari/)) {
-	// 	res.send('<h1>Hello Safari..! </h1>');
-	// }
-	// else {
-	// 	res.send('<h1>Hello Express..! </h1>');
-	// }
+	요청 헤더의 속성 추출
+	var agent = req.header('User-Agent');
+	// 브라우저 구분
+	if (agent.toLowerCase().match(/safari/)) {
+		res.send('<h1>Hello Safari..! </h1>');
+	}
+	else {
+		res.send('<h1>Hello Express..! </h1>');
+	}
 
-	// 요청 매개 변수 출력
-	// var name = req.param('name');
-	// var region = req.param('region');
-	// res.send('<h1>' + name + '-' + region + '!!</h1>');
+	요청 매개 변수 출력
+	var name = req.param('name');
+	var region = req.param('region');
+	res.send('<h1>' + name + '-' + region + '!!</h1>');
 
-// });
+});
 
 
 console.log("Web application opened");
