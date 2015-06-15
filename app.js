58ed94505c72ac330919f160b5dc4ec841ed5301
Jaehee Rhee <rjchaos@gmail.com>
2015-06-15 13:43:55 +0900

    send method 테스트

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
-// var http = require('http');
-// var express = require('express');
-// 
-// var port = process.env.PORT || 1337;
-// http.createServer(function(req, res) {
-//   res.writeHead(200, { 'Content-Type': 'text/plain' });
-//   res.end('Hello Azure World!! Error?\n');
-// }).listen(port);
-
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Node.js and Express!');
+	res.send('Hello Node.js and Express!');
+	var output = [];
+	for (var i = 0; i < 3; i++) {
+		output.push({
+			count: i,
+			name: 'name - ' + i
+		})
+	}
+	respond.send(output);
 });
 
 console.log("Web application opened");
 app.listen(process.env.PORT);
-
-// var server = app.listen(53327, function () {
-
-//   var host = server.address().address;
-//   var port = server.address().port;
-// 
-//   console.log('Example app listening at http://%s:%s', host, port);
-
-// });
