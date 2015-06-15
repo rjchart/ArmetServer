a578314cb847b88adf33e7aa5ca1224b2286d120
Jaehee Rhee <rjchaos@gmail.com>
2015-06-15 12:20:44 +0900

    change app.js for express test

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-var http = require('http');
+// var http = require('http');
+// var express = require('express');
+// 
+// var port = process.env.PORT || 1337;
+// http.createServer(function(req, res) {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello Azure World!! Error?\n');
+// }).listen(port);
+
 var express = require('express');
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Azure World!! Error?\n');
-}).listen(port);
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
