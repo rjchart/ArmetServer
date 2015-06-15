3328e01c69d1dd4c41c474a49ad0b3da210500fd
Jaehee Rhee <rjchaos@gmail.com>
2015-06-15 17:01:43 +0900

    path test 02

--- app.js
+++ app.js
@@ -37,9 +37,39 @@ app.use('/', function (req, res, next) {
 // 	// res.send('<h1>' + name + '-' + region + '!!</h1>');
 });
 
-app.use('/data', function(req,res,next) {
-	res.send('you typed data path');
+
+// 미들웨어를 생성합니다.
+app.use('/data/', function (req, res, next) {
+	// 기본적인 기능 테스트
+	res.send('Data test!');
+// 	next();
+// 	// 출력 테스트
+// 	// var output = [];
+// 	// for (var i = 0; i < 3; i++) {
+// 	// 	output.push({
+// 	// 		count: i,
+// 	// 		name: 'name - ' + i
+// 	// 	})
+// 	// }
+// 	// res.send(output);
+
+// 	// 요청 헤더의 속성 추출
+// 	// var agent = req.header('User-Agent');
+// 	// // 브라우저 구분
+// 	// if (agent.toLowerCase().match(/safari/)) {
+// 	// 	res.send('<h1>Hello Safari..! </h1>');
+// 	// }
+// 	// else {
+// 	// 	res.send('<h1>Hello Express..! </h1>');
+// 	// }
+
+// 	// 요청 매개 변수 출력
+// 	// var name = req.param('name');
+// 	// var region = req.param('region');
+// 	// res.send('<h1>' + name + '-' + region + '!!</h1>');
 });
+
+
 // app.use(express.logger());
 // app.use ('/static', express.static(__dirname + '/public/'));
 
