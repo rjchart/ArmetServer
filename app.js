98f16de3bd599f64f92c116212902d6d6ad3c26e
Jaehee Rhee <rjchaos@gmail.com>
2015-06-25 19:09:26 +0900

    test

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.get('/', function(request, response) {
 			if (!error) {
 				var testString = JSON.stringify(result.entries);
 				var newTest = JSON.parse(testString);
-				response.send(testString);
+				response.send(JSON.parse(newTest[0].name)._);
 				var users = ['geddy', 'neil', 'alex'];
 				// response.send(ejs.render(data, 
 				// 	{data: result.entries,
